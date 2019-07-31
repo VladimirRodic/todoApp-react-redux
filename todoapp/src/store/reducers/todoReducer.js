07671a3a9d0cd2105c4634ce9ab23b5441908341
Vladimir Rodic <vladimirrodic84@gmail.com>
2019-07-31 09:41:57 +0200

    removed comments in reducer

--- todoapp/src/store/reducers/todoReducer.js
+++ todoapp/src/store/reducers/todoReducer.js
@@ -3,11 +3,11 @@ export default (state = [], action) => {
     case "ADD_TODO":
       console.log("actionADD_TODO", action);
       console.log("state.todos undefined", state.todos);
-      /*      if (state.todos === undefined || state.todos.length === 0) {
+      if (state.todos === undefined || state.todos.length === 0) {
         state.todos = [];
         console.log("state.todos undefined", state.todos);
       }
-*/
+
       return [...state.todos, { text: action.text, completed: false }];
     default:
       return state;
